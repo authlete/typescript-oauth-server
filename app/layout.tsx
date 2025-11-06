6a5ed95b7f86cfc50c3f22820e3e179cae18d1f8
Zulfiqar Ahmed <zulfiqar.ahmed@gmail.com>
2025-11-07 00:11:35 +0500

    add password auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Providers } from "./providers";
+import { UserButton } from "@daveyplate/better-auth-ui";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +29,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Providers>
+        <header className="sticky top-0 z-50 flex h-12 justify-between border-b bg-background/60 px-safe-or-4 backdrop-blur md:h-14 md:px-safe-or-6">
+            <div className="flex items-center gap-2">
+                <UserButton size="icon" />
+            </div>
+        </header>
+          {children}
+        </Providers>
       </body>
     </html>
   );
